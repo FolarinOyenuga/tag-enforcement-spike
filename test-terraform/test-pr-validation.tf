63962fdac8f6d9ad5f7e7bc318510cd116b49167
folarin oyenuga <folarin.oyenuga@digital.justice.gov.uk>
2025-12-17 13:24:33 +0000

    Add test fixtures for tfsec tag validation

---
+++ test-terraform/test-pr-validation.tf
@@ -0,0 +1,17 @@
+# Test file to validate tfsec PR pipeline
+# This resource uses provider without default_tags to trigger failures
+
+resource "aws_sqs_queue" "missing_all_tags" {
+  provider = aws.no_default_tags
+  name     = "test-queue-no-tags"
+  # No tags inherited from provider - should fail all MOJ tag checks
+}
+
+resource "aws_lambda_function" "no_tags" {
+  provider      = aws.no_default_tags
+  function_name = "moj-lambda-no-tags"
+  role          = "arn:aws:iam::123456789012:role/lambda-role"
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  # No tags - should fail
+}
